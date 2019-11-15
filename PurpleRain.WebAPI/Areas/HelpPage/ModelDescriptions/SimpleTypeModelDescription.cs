d4a60616c95508cdb7cd922e759d82ab7da2e0de
Ian Mills <iandmills1@gmail.com>
2019-11-15 12:39:25 -0500

    second

---
+++ PurpleRain.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PurpleRain.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ PurpleRain.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PurpleRain.WebAPI.WebApiApplication" Language="C#" %>
