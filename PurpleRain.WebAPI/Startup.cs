78d9e58fe12be5cc1f93af3765ee43640cb0b3cf
ianmills1 <53943549+ianmills1@users.noreply.github.com>
2019-11-21 15:45:04 -0500

    added admin role

--- PurpleRain.WebAPI/Startup.cs
+++ PurpleRain.WebAPI/Startup.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
 using Owin;
+using PurpleRain.Data;
 
 [assembly: OwinStartup(typeof(PurpleRain.WebAPI.Startup))]
 
@@ -14,6 +17,35 @@ namespace PurpleRain.WebAPI
         {
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureAuth(app);
+            createAdminRole();
+        }
+
+        //Creating admin role
+        private void createAdminRole()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "UserAdmin";
+                user.Email = "useradmin@gmail.com";
+                string userPassword = "UserAdmin1!";
+
+                var checkUser = userManager.Create(user, userPassword);
+
+                if (checkUser.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, "Admin");
+                }
+            }
         }
     }
 }
